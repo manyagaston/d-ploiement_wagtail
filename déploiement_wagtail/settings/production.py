95848462e643989159182950d280dbf2a42a0768
junior manya <jmofficiel085@gmail.com>
2023-02-06 15:44:35 +0100

    commit du projet 1

--- déploiement_wagtail/settings/production.py
+++ déploiement_wagtail/settings/production.py
@@ -5,7 +5,7 @@ DEBUG = False
 SECRET_KEY = "esd+g0%f$2o+5)80gjg9dg798v#k0m%q7o%t7l9=q47&0%z-tj"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["déploiement_wagtail.eu.pythonanywhere.com"]
+ALLOWED_HOSTS = ["JManya.pythonanywhere.com "]
 
 ROOT_URLCONF = "déploiement_wagtail.urls"
 
